9f515e60149bbe2f9d8012c8a7716a7186a91459
tren-parasoft <109122853+tren-parasoft@users.noreply.github.com>
2025-07-31 10:20:23 +0800

    optimization for running tests (#17)

--- tests/runnner.spec.ts
+++ tests/runnner.spec.ts
@@ -142,7 +142,7 @@ describe('runnner', () => {
                 }
                 runOptions = {
                     report: reportPath + '.xml',
-                    parasoftToolOrJavaRootPath: path.join(__dirname, '/res/toolRootPaths/toolHome')
+                    parasoftToolOrJavaRootPath: process.env.JAVA_HOME || path.join(__dirname, '/res/toolRootPaths/toolHome')
                 };
             })
 
