cd3b1ae1bc7b41d8aec4c97c98cc2ed1bf56c8c0
Jason Chen <jchen@parasoft.com>
2025-08-18 11:09:04 +0800

    Change Flow and Duplicate to flow and duplicate

--- src/runner.ts
+++ src/runner.ts
@@ -227,7 +227,7 @@ export class StaticAnalysisParserRunner {
                 const descriptionLength = [...vulnerabilityDetailDescription].length;
                 const isFlowOrDuplicateViolation = ["FlowViol", "DupViol"].includes(result.partialFingerprints.violType);
                 if (isFlowOrDuplicateViolation) {
-                    const violationType = result.partialFingerprints.violType == "FlowViol" ? "Flow" : "Duplicate";
+                    const violationType = result.partialFingerprints.violType == "FlowViol" ? "flow" : "duplicate";
                     const additionalText = ` (${messagesFormatter.format(messages.flow_or_duplicate_violation_details_description, violationType)})`;
                     const additionalLength = [...additionalText].length;
                     if ((descriptionLength + additionalLength) > 2000) {
